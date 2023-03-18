5fae302dd26be9177fb6231f475c0f9c22a3de2d
codesentry17 <scrappycoco902@gmail.com>
2023-03-18 23:06:51 +0530

    notify admin

--- notify/admin.py
+++ notify/admin.py
@@ -8,4 +8,20 @@ class NotifyAdmin(admin.ModelAdmin):
     list_display_links = ('user_id','name')
     search_fields = ('user_id','name')
 
+    readonly_fields=[
+        'user_id',
+        'name', 
+        'created_date',
+        'brand',
+        'model',
+        'body_style',
+        'fuel',
+        'transmission',
+        'color',
+        'year',
+        'milage',
+        'min_price',
+        'max_price',
+    ]
+
 admin.site.register(Specification, NotifyAdmin)
