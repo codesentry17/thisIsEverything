c3d8ab4d66151217f7711724afad66d7864ac783
codesentry17 <123292957+codesentry17@users.noreply.github.com>
2023-04-05 01:40:08 +0530

    Update settings.py

--- carzone/settings.py
+++ carzone/settings.py
@@ -165,7 +165,7 @@ SITE_ID = 1
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'fordjangoproj2@gmail.com'
-EMAIL_HOST_PASSWORD = 'hwxeoktgiqvdlkgq'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 
